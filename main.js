0551362176f7af53ed7a2e820fc522b71d6886c3
chry902 <tchry902@gmail.com>
2021-10-08 17:18:49 +0200

    avanzamento

--- main.js
+++ main.js
@@ -5,14 +5,14 @@
 
 const user_name = prompt("come ti chiami?");
 
-console.log("dati", + Christopher);
+console.log("dati", + user_name);
 
 const last_name = prompt("qual è il tuo cognome?");
-console.log("dati", + Tarascio);
+console.log("dati", + last_name);
 
 
 const preferit_color = prompt("qual è il tuo colore preferito?");
-console.log("dati", + Black);
+console.log("dati", + preferit_color);
 
 
 document.getElementById(`dati`).innerHTML = "dati", + user_name, + last_name, + preferit_color;
