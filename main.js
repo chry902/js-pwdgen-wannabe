c79534d5ad3871a393870eafc03234e0585cc3f6
chry902 <tchry902@gmail.com>
2021-10-08 16:12:32 +0200

    inizio esercizio

---
+++ main.js
@@ -0,0 +1,8 @@
+const user_name; Christopher
+const last_name; Tarascio
+const preferit_color; black
+
+console.log(`dati = + name + surname + preferit_color`);
+
+
+document.getElementById(`dati`).innerHTML = `dati ` + user_name + last_name + preferit_color;
