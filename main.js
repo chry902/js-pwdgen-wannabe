31c77e7b7705f198953ac1a8c0a7255c117604d4
chry902 <tchry902@gmail.com>
2021-10-09 16:13:17 +0200

    funzione completata

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ const user_name = prompt("come ti chiami?");
 console.log("dati", + user_name);
 
 const last_name = prompt("qual è il tuo cognome?");
+
 console.log("dati", + last_name);
 
 
 const preferit_color = prompt("qual è il tuo colore preferito?");
+
 console.log("dati", + preferit_color);
 
 
-document.getElementById(`dati`).innerHTML = "dati", + user_name, + last_name, + preferit_color;
+document.getElementById("dati").innerHTML = user_name + last_name + preferit_color + 21;
